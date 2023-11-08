1b62eeba9ddf2e90710b7879677d385373638610
De-lazzari Alexis <alexis.de-lazzari9@etu.univ-lorraine.fr>
2023-11-08 16:39:56 +0100

    ajout des screen hebdomadaire d'analyse

---
+++ .bat
@@ -0,0 +1,1 @@
+mvn clean verify sonar:sonar -Dsonar.projectKey=banquetest -Dsonar.projectName='banquetest' -Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_b916c8b1bb029fbf02aff02d0269a537cc23a53f
